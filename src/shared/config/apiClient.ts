7ac5580b20e2c0d978f71a45db8cfcbe95dc9005
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-03-23 21:07:42 +0500

    fix: fix base url

--- src/shared/config/apiClient.ts
+++ src/shared/config/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://xn----7sbhlqzghjcdke5k.xn--p1ai/api";
-const BASE_URL = "http://localhost:6001/";
+const BASE_URL = "https://xn----7sbhlqzghjcdke5k.xn--p1ai/api";
+// const BASE_URL = "http://localhost:6001/";
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
