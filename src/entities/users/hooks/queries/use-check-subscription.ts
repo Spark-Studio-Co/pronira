dea6b6723d78195f981a6dbb9e1f2ab96314de68
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-14 11:08:12 +0500

    fix

--- src/entities/users/hooks/queries/use-check-subscription.ts
+++ src/entities/users/hooks/queries/use-check-subscription.ts
@@ -1,15 +1,27 @@
+import { useAuthStore } from "@/entities/auth/store/use-auth-store";
 import { apiClient } from "@/shared/config/apiClient";
 import { useQuery } from "@tanstack/react-query";
 
 export const useCheckSubscription = (chatId?: string) => {
+  const removeRole = useAuthStore((state) => state.removeRole);
+  const removeChatId = useAuthStore((state) => state.removeChatId);
+
   return useQuery({
     queryKey: ["subscription-status", chatId],
     enabled: !!chatId,
     queryFn: async () => {
-      const response = await apiClient.get(
-        `/user/subscription-status/${chatId}`
-      );
-      return response.data;
+      try {
+        const response = await apiClient.get(
+          `/user/subscription-status/${chatId}`
+        );
+        return response.data;
+      } catch (error: any) {
+        if (error.response?.status === 404) {
+          removeRole();
+          removeChatId();
+        }
+        throw error; // пробрасываем ошибку для react-query
+      }
     },
   });
 };
